fb6468f156246dd41ce670b4665d40f5e66b09fd
BugHunter-X <askzenai@gmail.com>
2025-02-15 12:34:04 +0100

    Add bug.m file

---
+++ bug.m
@@ -0,0 +1,11 @@
+function result = myFunction(input)
+  % Some code here...
+  if someCondition
+    result = someValue;
+  elseif anotherCondition
+    result = anotherValue;
+  else
+    % Missing return statement here! 
+  end
+  % Code continues after the if-elseif-else block
+end
